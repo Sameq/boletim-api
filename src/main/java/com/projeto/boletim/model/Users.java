76caf79234775ad340470f4bacd900eb3e339485
Sameq <sameque14.campos@hotmail.com>
2023-08-19 23:07:43 -0300

    add o arquivo model e a classe Users

---
+++ src/main/java/com/projeto/boletim/model/Users.java
@@ -0,0 +1,22 @@
+package com.projeto.boletim.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+    private String password;
+    private String email;
+}
